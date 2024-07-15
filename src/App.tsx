21164fe4cc83eea6e2346bda5aeff42a9d542c93
cjmunozy <cjmunoz@espol.edu.ec>
2024-07-14 19:39:00 -0500

    Agregada la predicción
    Se agregó una tabla que muestra las predicciones climáticas de los siguientes 5 días

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,20 @@ import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import './App.css'
 //import Indicator from './components/Indicator';
 import Summary from './components/Summary';
-import BasicTable from './components/BasicTable';
+//import BasicTable from './components/BasicTable';
 import WeatherChart from './components/WeatherChart';
 import ControlPanel from './components/ControlPanel';
 import Information from './components/Information';
+import Indicator from './components/Indicator';
+import ForecastTable from './components/ForecastTable';
+
+interface Row {
+  rango: string;
+  temperatura: string;
+  sensacion: string;
+  humedad: string;
+  presion: string;
+}
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -17,6 +27,10 @@ function App() {
 
   let [informacion, setInformacion] = useState<React.ReactNode[]>([])
 
+  let [now, setNow] = useState<React.ReactNode[]>([])
+
+  let [rowsTable, setRowsTable] = useState<Row[]>([])
+
   {/* Hook: useEffect */}
 	
   useEffect(
@@ -24,9 +38,10 @@ function App() {
       () => {
           (async () => {
           
-              {/* Del LocalStorage, obtiene el valor de las claves openWeatherMap y expiringTime */}
+              {/* Del LocalStorage, obtiene el valor de las claves forecastOpenWeatherMap y expiringTime */}
 
-              let savedTextXML = localStorage.getItem("openWeatherMap")!
+              let nowTextXML = localStorage.getItem("nowOpenWeatherMap")!
+              let forecastTextXML = localStorage.getItem("forecastOpenWeatherMap")!
               let expiringTime = localStorage.getItem("expiringTime")
 
               {/* Estampa de tiempo actual */}
@@ -42,9 +57,10 @@ function App() {
                   {/* Request */}
               
                   let API_KEY = "fe14d460788eb1506d194b4ed0ce3c80";
-                  let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`);
-                  savedTextXML = await response.text();
-              
+                  let nowResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Guayaquil&mode=xml&appid=${API_KEY}`);
+                  nowTextXML = await nowResponse.text();
+                  let forecastResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`);
+                  forecastTextXML = await forecastResponse.text();
               
                   {/* Diferencia de tiempo */}
               
@@ -52,63 +68,120 @@ function App() {
                   let delay = hours * 3600000
               
               
-                  {/* En el LocalStorage, almacena texto en la clave openWeatherMap y la estampa de tiempo de expiración */}
+                  {/* En el LocalStorage, almacena texto en la clave forecastOpenWeatherMap y la estampa de tiempo de expiración */}
               
-                  localStorage.setItem("openWeatherMap", savedTextXML)
+                  localStorage.setItem("nowOpenWeatherMap", nowTextXML)
+                  localStorage.setItem("forecastOpenWeatherMap", forecastTextXML)
                   localStorage.setItem("expiringTime", (nowTime + delay ).toString() )
               }
           
               {/* Request
               
               let API_KEY = "fe14d460788eb1506d194b4ed0ce3c80";
-              let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`);
-              let savedTextXML = await response.text();
+              let forecastResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`);
+              let forecastTextXML = await forecastResponse.text();
               */}
               
               {/* XML Parser */}
               
               const parser = new DOMParser();
-              const xml = parser.parseFromString(savedTextXML, "application/xml");
+              const nowXml = parser.parseFromString(nowTextXML, "application/xml");
+              const forecastXml = parser.parseFromString(forecastTextXML, "application/xml")
               
               {/* Arreglo para agregar los resultados */}
               
               let informacion = new Array()
+              let now = new Array()
 
               {/* 
                   Análisis, extracción y almacenamiento del contenido del XML 
                   en el arreglo de resultados
               */}
 
-              let datos = xml.getElementsByTagName("location")[0]
-              let localidad = xml.getElementsByTagName("location")[1]
+              let datos = nowXml.getElementsByTagName("current")[0]
 
-              let ciudad = datos.querySelector("name")?.textContent
+              let ciudad = datos.querySelector("city")!.getAttribute("name")
               informacion.push(["Ciudad", ciudad])
 
               let pais = datos.querySelector("country")?.textContent
               informacion.push(["Pais", pais])
 
-              let zona = ["Zona horaria", "GMT -5"]
+              let zona = ["Zona Horaria", "GMT -5"]
               informacion.push(zona)
 
-              let geobase = localidad.getAttribute("geobaseid")
-              informacion.push(["Geobaseid", geobase])
+              let geobaseid = datos.querySelector("city")!.getAttribute("id")
+              informacion.push(["Geobaseid", geobaseid])
               
-              let latitud = localidad.getAttribute("latitude")
+              let latitud = datos.querySelector("coord")!.getAttribute("lat")
               informacion.push(["Latitud", latitud])
               
-              let longitud = localidad.getAttribute("longitude")
+              let longitud = datos.querySelector("coord")!.getAttribute("lon")
               informacion.push(["Longitud", longitud])
+
+              let temperaturaString = datos.querySelector("temperature")!.getAttribute("value")
+              let temperatura = (Number.parseFloat(temperaturaString!) - 273.15).toPrecision(4)
+              now.push(["Temperatura", "° C" , temperatura])
+
+              let sensacionString = datos.querySelector("feels_like")!.getAttribute("value")
+              let sensacion = (Number.parseFloat(sensacionString!) - 273.15).toPrecision(4)
+              now.push(["Sensación Térmica", "° C" , sensacion])
+
+              let humedad = datos.querySelector("humidity")!.getAttribute("value")
+              let unidad = datos.querySelector("humidity")!.getAttribute("unit")
+              now.push(["Humedad", unidad , humedad])
+
+              let presion = datos.querySelector("pressure")!.getAttribute("value")
+              unidad = datos.querySelector("pressure")!.getAttribute("unit")
+              now.push(["Presión Atmosférica", unidad , presion])
+
+              let velocidad = datos.querySelector("speed")!.getAttribute("value")
+              unidad = datos.querySelector("speed")!.getAttribute("unit")
+              now.push(["Velocidad", unidad , velocidad])
+
+              let direccion = datos.querySelector("direction")!.getAttribute("value")
+              unidad = "° " + datos.querySelector("direction")!.getAttribute("name")
+              now.push(["Dirección", unidad , direccion])
+
+              let predicciones = forecastXml.getElementsByTagName("time")
+              let arrayPredicciones = Array.from(predicciones).map( (prediccion) => {
+                let inicio = prediccion.getAttribute("from")!
+                let fin = prediccion.getAttribute("to")!
+                let rango = inicio.replace("T", " ") + " - " + fin.replace("T", " ")
+                let temperaturaString = prediccion.querySelector("temperature")!.getAttribute("value")!
+                let temperatura = (Number.parseFloat(temperaturaString) - 273.15).toPrecision(4)
+                let sensacionString = prediccion.querySelector("feels_like")!.getAttribute("value")
+                let sensacion = (Number.parseFloat(sensacionString!) - 273.15).toPrecision(4)
+                let humedad = prediccion.querySelector("humidity")!.getAttribute("value")
+                let presion = prediccion.querySelector("pressure")!.getAttribute("value")
+                unidad = datos.querySelector("pressure")!.getAttribute("unit")
+                let row : Row = {
+                  "rango": rango,
+                  "temperatura": temperatura,
+                  "sensacion": sensacion,
+                  "humedad": humedad!,
+                  "presion": presion!
+                }
+                return row
+              })!
+              
               
               {/* Renderice el arreglo de resultados en un arreglo de elementos Indicator */}
               
               let informacionElements = Array.from(informacion).map(
                 (element) => <Information title={element[0]} value={element[1]} />
               )
+
+              let nowElements = Array.from(now).map(
+                (element) => <Indicator title={element[0]} subtitle={element[1]} value={element[2]} />
+              )
             
               {/* Modificación de la variable de estado mediante la función de actualización */}
               
               setInformacion(informacionElements)
+
+              setNow(nowElements)
+
+              setRowsTable(arrayPredicciones)
             
             })()    
       },
@@ -143,22 +216,45 @@ function App() {
         </section>
         <section id="actual">
           <h1>Clima actual</h1>
+          <Grid container spacing={5}>
+            <Grid xs={6} sm={6} md={3} lg={3}>
+                {now[0]}
+            </Grid>
+            <Grid xs={6} sm={6} md={3} lg={3}>
+                {now[1]}
+            </Grid>
+            <Grid xs={6} sm={6} md={3} lg={3}>
+                {now[2]}
+            </Grid>
+            <Grid xs={6} sm={6} md={3} lg={3}>
+                {now[3]}
+            </Grid>
+          </Grid>
+          <h2>Viento</h2>
+          <Grid container spacing={10} justifyContent={'center'}>
+            <Grid xs={6} sm={6} md={3} lg={3}>
+                {now[4]}
+            </Grid>
+            <Grid xs={6} sm={6} md={3} lg={3}>
+                {now[5]}
+            </Grid>
+          </Grid>
         </section>
         <section id ="prediccion">
-          <h1>Próximos 3 días</h1>
+          <h1>Próximos 5 días</h1>
+          <ForecastTable rows={rowsTable}></ForecastTable>
         </section>
         <section id="resumen">
           <h1>Resumen</h1>
         </section>
         <Grid container spacing={5}>
-            <Summary></Summary>
-            <BasicTable></BasicTable>
             <Grid xs={12} lg={2}>
               <ControlPanel />
             </Grid>
             <Grid xs={12} lg={10}>
               <WeatherChart></WeatherChart>
             </Grid>
+            <Summary></Summary>
 	      </Grid>
     </>
   )
---
+++ src/components/ForecastTable.tsx
@@ -0,0 +1,98 @@
+//import * as React from 'react';
+import { useState, useEffect} from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+/*
+function createData(
+  rango: string,
+  temperatura: number,
+  sensacion: number,
+  humedad: number,
+  presion: number,
+) {
+  return { rango, temperatura, sensacion, humedad, presion };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+*/
+
+interface Row {
+  rango: string;
+  temperatura: string;
+  sensacion: string;
+  humedad: string;
+  presion: string;
+}
+
+interface Config {
+  rows: Array<Row>
+}
+ 
+export default function ForecastTable(data: Config) {
+
+  {/* 
+    Declare la variable de estado (rows) y la función de actualización (setRows).
+    Use el mismo identificador de la variable con valores fijos (rows)
+  */}
+
+  let [rows, setRows] = useState<Row[]>([])
+
+  {/* 
+      Agregue el hook useEffect, controlado por el prop del componente (data), y
+      Dentro del hook, invoque al métdo de actualización con el valor del prop (data.rows).
+  */}
+
+  useEffect( () => {
+
+    (()=> {
+
+        setRows(data.rows)
+
+    })()
+
+}, [data] )
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align='center'>Rango de horas</TableCell>
+            <TableCell align="center">Temperatura (°C)</TableCell>
+            <TableCell align="center">Sensación Térmica (°C)</TableCell>
+            <TableCell align="center">Humedad (%)</TableCell>
+            <TableCell align="center">Presión (hPa)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.rango}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row" align='center'>
+                {row.rango}
+              </TableCell>
+              <TableCell align="center">{row.temperatura}</TableCell>
+              <TableCell align="center">{row.sensacion}</TableCell>
+              <TableCell align="center">{row.humedad}</TableCell>
+              <TableCell align="center">{row.presion}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
